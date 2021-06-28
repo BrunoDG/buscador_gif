4513a84c41b82b3ebc104e916e499113e5ddaf80
Bruno Dias <brunodg.dev@gmail.com>
2021-06-28 20:29:01 -0300

    App 5 terminado.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart';
-
 import 'package:buscador_gif/ui/home_page.dart';
+import 'package:buscador_gif/ui/gif_page.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    //home: GifPage(),
+    theme: ThemeData(hintColor: Colors.white),
   ));
 }
- 
